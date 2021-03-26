5502f22f190f1f5a0bfb93ba2b91febe76e54148
Raphael <veauville.raphael@gmail.com>
2021-03-26 21:37:07 +0100

    Update

--- main.js
+++ main.js
@@ -1,76 +1,50 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const mongoose = require('mongoose');
 const morgan = require('morgan')("dev");
+const db = require('./util/db')
 
 const server = http.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
+  console.log('Server started on port', server.address().port);
 });
 
 
-const mongOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 10000, // Keep trying to send operations for 5 seconds //5000
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity  //45000
-    family: 4 // Use IPv4, skip trying IPv6
-  }
-const dbUrl = "mongodb://localhost:27017/instantchat";
-const Message = mongoose.model('Message',{
-    name : String,
-    message : String
-  })
-mongoose.connect(dbUrl ,mongOptions ,(err) => {
-    console.log('mongodb connected',err);
-})
-  
 
-app.use(express.static(__dirname +"/front-end"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(__dirname + "/front-end"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use(morgan)
 
 
 app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
+  db.query('SELECT * FROM messages LIMIT 200', (err, result) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('An error has occurred.')
+    }
+    res.send(result);
   })
 })
 
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
 
 app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err) {
-        sendStatus(500);
-        return console.log("error")
+  if (!req.body.name || req.body.name && req.body.name.trim() === '') return res.status(400).send('Missing name value in body.')
+  if (!req.body.message || req.body.message && req.body.message.trim() === '') return res.status(400).send('Missing message value in body.')
+  db.query('INSERT INTO messages (name, message) VALUES (?, ?)', [req.body.name, req.body.message], (err, result) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('An error has occurred.')
     }
     io.emit('message', req.body);
-    res.sendStatus(200);
-    /*try{
-       return res.redirect('http://localhost:3000');
-
-    }catch(err){
-        console.log(err)
-    }*/
+    res.status(200).send('success');
   })
 })
 
-io.on('connection', () =>{
+io.on('connection', () => {
   console.log('A user is connected')
 })
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.send('Page not found', 404);
 });
---
+++ util/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const config = require('../config')
+const connection = mysql.createConnection({
+	host: config.database.host,
+	user: config.database.user,
+	password: config.database.password,
+	database: config.database.name
+});
+connection.connect(function (err) {
+	if (err) {
+		console.error('Mysql error connecting: ' + err.stack);
+		return;
+	}
+	console.log('Mysql connected as id: ' + connection.threadId);
+});
+
+module.exports = connection;
